b4baf2bca3279bb2f2beb2e1186b10f4b9cd1f3e
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-04 14:07:51 -0700

    add getters for beanproduct

--- src/test/java/BeanProductTest.java
+++ src/test/java/BeanProductTest.java
@@ -17,4 +17,31 @@ public class BeanProductTest {
     BeanProduct testBeanProduct = new BeanProduct("Spring Fling", 14, "Mexico", "City Roast");
     assertTrue(testBeanProduct instanceof BeanProduct);
   }
+
+  @Test
+  public void beanProduct_instantiateswithName_true(){
+    BeanProduct testBeanProduct = new BeanProduct("Spring Fling", 14, "Mexico", "City Roast");
+    assertEquals("Spring Fling", testBeanProduct.getName());
+  }
+
+  @Test
+  public void beanProduct_instantiateswithPrice_true(){
+    BeanProduct testBeanProduct = new BeanProduct("Spring Fling", 14, "Mexico", "City Roast");
+    final float DELTA = 1;
+    assertEquals(14, testBeanProduct.getPrice(), DELTA);
+  }
+
+  @Test
+  public void beanProduct_instantiateswithOrigin_true(){
+    BeanProduct testBeanProduct = new BeanProduct("Spring Fling", 14, "Mexico", "City Roast");
+    assertEquals("Mexico", testBeanProduct.getOrigin());
+  }
+
+  @Test
+  public void beanProduct_instantiateswithRoast_true(){
+    BeanProduct testBeanProduct = new BeanProduct("Spring Fling", 14, "Mexico", "City Roast");
+    assertEquals("City Roast", testBeanProduct.getRoast());
+  }
+
+
 }
